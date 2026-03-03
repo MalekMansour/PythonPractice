70792605cd56428f30d5e5422a1aa3db15c7a7f3
Malek Mansour <138655005+MalekMansour@users.noreply.github.com>
2026-03-02 19:39:21 -0700

    Create sudoku.py

---
+++ practice5/sudoku.py
@@ -0,0 +1,56 @@
+def is_valid(board, row, col, num):
+    # Check row
+    if num in board[row]:
+        return False
+    
+    # Check column
+    if num in [board[i][col] for i in range(9)]:
+        return False
+    
+    # Check 3x3 box
+    box_row, box_col = 3 * (row // 3), 3 * (col // 3)
+    for i in range(box_row, box_row + 3):
+        for j in range(box_col, box_col + 3):
+            if board[i][j] == num:
+                return False
+    
+    return True
+
+def solve(board):
+    for row in range(9):
+        for col in range(9):
+            if board[row][col] == 0:
+                for num in range(1, 10):
+                    if is_valid(board, row, col, num):
+                        board[row][col] = num
+                        if solve(board):
+                            return True
+                        board[row][col] = 0
+                return False
+    return True
+
+def print_board(board):
+    for i in range(9):
+        if i % 3 == 0 and i != 0:
+            print("------+-------+------")
+        for j in range(9):
+            if j % 3 == 0 and j != 0:
+                print("|", end=" ")
+            print(board[i][j], end=" ")
+        print()
+
+# Example usage
+sudoku_board = [
+    [5,3,0,0,7,0,0,0,0],
+    [6,0,0,1,9,5,0,0,0],
+    [0,9,8,0,0,0,0,6,0],
+    [8,0,0,0,6,0,0,0,3],
+    [4,0,0,8,0,3,0,0,1],
+    [7,0,0,0,2,0,0,0,6],
+    [0,6,0,0,0,0,2,8,0],
+    [0,0,0,4,1,9,0,0,5],
+    [0,0,0,0,8,0,0,7,9]
+]
+
+solve(sudoku_board)
+print_board(sudoku_board)
